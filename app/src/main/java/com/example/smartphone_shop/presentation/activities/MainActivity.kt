19f82a2bb0f8695bea571a8c825a113184aac0fa
bakunovgerman <german.bakunov@gmail.com>
2021-09-13 20:06:22 +0700

    переверстал bottomNavigationView

--- app/src/main/java/com/example/smartphone_shop/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/smartphone_shop/presentation/activities/MainActivity.kt
@@ -2,6 +2,13 @@ package com.example.smartphone_shop.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -15,14 +22,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), FragmentClickListener {
 
-    private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var navController: NavController
     private lateinit var cartViewModel: CartViewModel
+    private lateinit var cartImageView: ImageView
+    private lateinit var countItemsCartTextView: TextView
+    private lateinit var bottomNavigationView: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        cartViewModel = ViewModelProvider(this).get(CartViewModel::class.java)
         initView()
         initListener()
         initSubscribe()
@@ -31,73 +41,65 @@ class MainActivity : AppCompatActivity(), FragmentClickListener {
     }
 
     private fun initListener() {
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.main -> {
-                    if (navController.currentDestination?.id == R.id.detailFragment) {
-                        navController.navigate(R.id.action_detail_fragment_to_main_fragment)
-                    } else if (navController.currentDestination?.id == R.id.cartFragment) {
-                        navController.navigate(R.id.action_cart_fragment_to_main_fragment)
-                    }
-                    true
-                }
-                R.id.cart -> {
-                    if (navController.currentDestination?.id == R.id.mainFragment) {
-                        navController.navigate(R.id.action_main_fragment_to_cart_fragment)
-                    } else if (navController.currentDestination?.id == R.id.detailFragment) {
-                        navController.navigate(R.id.action_detail_fragment_to_cart_fragment)
-                    }
-                    true
-                }
-                else -> false
-            }
+        cartImageView.setOnClickListener {
+            navController.navigate(R.id.cartFragment)
+            bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout)
         }
     }
 
     private fun initView() {
-        cartViewModel = ViewModelProvider(this).get(CartViewModel::class.java)
-        bottomNavigationView = findViewById(R.id.bottom_navigation)
         navController = findNavController(R.id.nav_host)
-        bottomNavigationView.setupWithNavController(navController)
+        cartImageView = findViewById(R.id.imgCart)
+        countItemsCartTextView = findViewById(R.id.tvCountItemsCart)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
     }
 
     private fun initSubscribe() {
         cartViewModel.cartInfo.observe(this, Observer(::setCountItemCart))
     }
 
-
     private fun setCountItemCart(cartResponseItem: CartResponseItem) {
-        bottomNavigationView.getOrCreateBadge(R.id.cart).apply {
-            number = cartResponseItem.basket.size
-            if (number > 0)
-                isVisible = true
+        if (cartResponseItem.basket.isNotEmpty()) {
+            countItemsCartTextView.visibility = View.VISIBLE
+            countItemsCartTextView.text = cartResponseItem.basket.size.toString()
         }
     }
 
     override fun onBackPressed() {
-        if (navController.currentDestination?.id == R.id.detailFragment)
+        if (navController.currentDestination?.id == R.id.detailFragment){
             navController.popBackStack()
-        else
+            bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout_rounded)
+        }
+        else {
             super.onBackPressed()
+        }
     }
 
     override fun onOpenDetailFragmentClick() {
         navController.navigate(R.id.detailFragment)
+        bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout_rounded)
     }
 
     override fun onBackFragmentClick() {
-        if (navController.currentDestination?.id == R.id.detailFragment)
+        if (navController.currentDestination?.id == R.id.detailFragment){
             navController.navigate(R.id.mainFragment)
+            bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout_rounded)
+        }
         else if (navController.currentDestination?.id == R.id.cartFragment
-            && navController.previousBackStackEntry?.destination?.id == R.id.detailFragment)
+            && navController.previousBackStackEntry?.destination?.id == R.id.detailFragment) {
             navController.navigate(R.id.action_cart_fragment_to_detail_fragment)
+            bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout_rounded)
+        }
         else if (navController.currentDestination?.id == R.id.cartFragment
-            && navController.previousBackStackEntry?.destination?.id == R.id.mainFragment)
+            && navController.previousBackStackEntry?.destination?.id == R.id.mainFragment) {
             navController.popBackStack()
+            bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout_rounded)
+        }
     }
 
     override fun onOpenCartFragmentClick() {
         navController.navigate(R.id.cartFragment)
+        bottomNavigationView.background = ContextCompat.getDrawable(this, R.drawable.bg_bottom_navigation_layout)
     }
 
 }
